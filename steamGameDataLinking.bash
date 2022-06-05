166ffb0f306a5a638fddd927f5b0a7ef36b0e79f
Deckweiss <Deckweiss75@gmail.com>
2022-06-05 03:36:30 +0200

    Added drive C and restructured the controlFlow

--- steamGameDataLinking.bash
+++ steamGameDataLinking.bash
@@ -4,7 +4,7 @@ LINKS_GO_HERE=~/Steam
 STEAM_COMPATDATA_PATH=~/.local/share/Steam/steamapps/compatdata
 STEAM_APPMANIFEST_PATH=~/.local/share/Steam/steamapps
 
-read -r -d '' STEAM_IGNORE_DIRECTORIES << EOM
+read -r -d '' STEAM_IGNORE_DIRECTORIES <<EOM
 Downloads
 Music
 My Music
@@ -17,11 +17,11 @@ Microsoft
 CryptnetUrlCache
 EOM
 
-function containsElement () {
+function containsElement() {
   local e match="$1"
   shift
   for e; do
-    [[ "$e" == "$match" ]] && return 0;
+    [[ "$e" == "$match" ]] && return 0
   done
   return 1
 }
@@ -33,11 +33,17 @@ function createLink() {
   echo "Created Link $parentDir -> $targetDir"
 }
 
+function createLinkIfTargetExists() {
+  local parentDir="$1"
+  local targetDir="$2"
+  [[ ! -d "$targetDir" ]] && echo "Target directory does not exist: $targetDir" && return
+  createLink "$parentDir" "$targetDir"
+}
+
 function createLinksWhenRelevant() {
   local appName="$1"
   local directory="$2"
-  local targetParentDirName
-  targetParentDirName="$(basename -- "$directory")"
+  local targetParentDirName="${3:-$(basename -- "$directory")}"
 
   local listOfSubdirs
   listOfSubdirs="$(ls "$directory" | grep -vFx "$STEAM_IGNORE_DIRECTORIES")"
@@ -54,55 +60,65 @@ function createLinksWhenRelevant() {
   done
 }
 
+function createUserdataLink() {
+  echo "hi"
+}
+
 function updateGameLinks() {
   local appName="$1"
   local directory="$2"
 
-  local documentsPath="$directory"pfx/drive_c/users/steamuser/Documents
+  mkdir -p "$LINKS_GO_HERE/$appName"
+
+  local driveC="$directory"/pfx/drive_c
+  createLinkIfTargetExists "$LINKS_GO_HERE/$appName/Compatdata_DriveC" "$driveC"
+
+  local documentsPath="$directory"/pfx/drive_c/users/steamuser/Documents
   # When documentPath that we assumed does not exists, skip it
   if [[ -d "$documentsPath" && ! -L "$documentsPath" ]]; then
-    createLinksWhenRelevant "$appName" "$documentsPath"
+    createLinksWhenRelevant "$appName" "$documentsPath" "Compatdata_Documents"
   else
     echo "No documents directory found"
   fi
 
-  local myDocumentsPath="$directory"pfx/drive_c/users/steamuser/My\ Documents
+  local myDocumentsPath="$directory"/pfx/drive_c/users/steamuser/My\ Documents
   # When my documents path that we assumed does not exists, skip it
   if [[ -d "$myDocumentsPath" && ! -L "$myDocumentsPath" ]]; then
-    createLinksWhenRelevant "$appName" "$myDocumentsPath"
+    createLinksWhenRelevant "$appName" "$myDocumentsPath" "Compatdata_My_Documents"
   else
     echo "No my documents directory found"
   fi
 
-  local appdataPath="$directory"pfx/drive_c/users/steamuser/AppData/LocalLow
+  local appdataPath="$directory"/pfx/drive_c/users/steamuser/AppData/LocalLow
   # When appdata that we assumed does not exists, skip it
   if [[ -d "$appdataPath" && ! -L "$appdataPath" ]]; then
-    createLinksWhenRelevant "$appName" "$appdataPath"
+    createLinksWhenRelevant "$appName" "$appdataPath" "Compatdata_AppData"
   else
     echo "No appdata directory found"
   fi
+
+  local appdataPath="$directory"/pfx/drive_c/users/steamuser/AppData/Roaming
+    # When appdata that we assumed does not exists, skip it
+    if [[ -d "$appdataPath" && ! -L "$appdataPath" ]]; then
+      createLinksWhenRelevant "$appName" "$appdataPath" "Compatdata_AppData"
+    else
+      echo "No appdata directory found"
+    fi
 }
 
 function update() {
-  local directory
-  for directory in "$STEAM_COMPATDATA_PATH"/*/; do
-    # When not a directory, skip it
-    [[ -L "${directory%/}" ]] && echo "Not a directory: $directory" && continue
-
-    local appid="$(basename -- "$directory")"
-    local appManifest="$STEAM_APPMANIFEST_PATH/appmanifest_$appid.acf"
-
-    # When no appManifest found, skip it
-    [[ ! -f "$appManifest" ]] && echo "No appmanifest found for: $appid" && continue
-
+  local appManifest
+  for appManifest in "$STEAM_APPMANIFEST_PATH/"*.acf; do
+    local appid="$(grep -oP '(?<="appid"\t\t).*' "$appManifest" | sed 's/"//g')"
     local appName="$(grep -oP '(?<="name"\t\t).*' "$appManifest" | sed 's/"//g')"
+    local compatdataDir="$STEAM_COMPATDATA_PATH/$appid"
 
     # When Proton, skip it
     [[ $appName == Proton* ]] && echo "Ignored because this: $appid is called: $appName" && continue
 
-    echo "Working on - id: $appid name: $appName"
+    echo "Working on - id: $appid name: $appName compatdata: $compatdataDir"
 
-    updateGameLinks "$appName" "$directory"
+    updateGameLinks "$appName" "$compatdataDir"
 
     echo " "
     echo " "
