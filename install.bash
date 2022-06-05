1797a5e54c7af5ecaf44a73e279904b670fca427
Deckweiss <Deckweiss75@gmail.com>
2022-06-05 16:18:22 +0200

    Added install script

---
+++ install.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+mkdir ~/.steamGameDataLinking
+cd ~/.steamGameDataLinking
+curl https://raw.githubusercontent.com/Deckweiss/SteamLinuxGameDataLinking/main/steamGameDataLinking.bash -O
+chmod +x ~/.steamGameDataLinking/steamGameDataLinking.bash
+
+cd ~/.config/autostart/
+curl https://raw.githubusercontent.com/Deckweiss/SteamLinuxGameDataLinking/main/steamGameDataLinking.desktop -O
+chmod +x ~/.steamGameDataLinking/steamGameDataLinking.desktop
+
+#curl https://raw.githubusercontent.com/Deckweiss/SteamLinuxGameDataLinking/main/install.bash | bash
